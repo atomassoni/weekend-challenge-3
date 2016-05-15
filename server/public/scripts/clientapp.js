6e718518fe0362c875f0db37edf5722604012404
atomassoni <annetomassoni@gmail.com>
2016-05-15 18:23:18 -0500

    fixed bugs

--- server/public/scripts/clientapp.js
+++ server/public/scripts/clientapp.js
@@ -1,24 +1,31 @@
 var number1 = '';
 var number2 = '';
 var operator = '';
-var prevOperator = '';
+
 var operatorDisplay = {
     'add': '+',
     'subtract': '-',
     'multiply': 'x',
     'divide': '/'
 };
-var displayCalcString = '';
+
 var currentNumber = 1;
+
+
 
 $(document).ready(function() {
     console.log('ready');
 
 
     $('#calculator-container').on('click', 'button', calculatorButtons);
+
     $('#number1').on('keyup', updateNum);
     $('#number2').on('keyup', updateNum);
+
     $('#calc-input').on('submit', getResult);
+    $('#clear').on('click', clearAllNumbers);
+    $('#clear-input').on('click', clearInputOnly);
+
 
     //When the buttons are pressed, changes the input fields and variables
     function calculatorButtons() {
@@ -32,7 +39,7 @@ $(document).ready(function() {
             $('#display-operator').text(' ' + operatorDisplay[operator] + ' ');
 
         } else {
-            if (operator == "") {
+            if (operator == "" && currentNumber==1) {
                 number1 += buttonPushed;
                 $('#number').val(number1);
 
@@ -54,67 +61,65 @@ $(document).ready(function() {
         }
     }
 
-    function calculate() {
-        event.preventDefault();
 
-        $.ajax({
-                type: 'GET',
-                url: '/' + operator,
-
-                success: function(data) {
-
-                    $('#result').text(data.solution);
-                }
-            });
-    }
 
     function getResult() {
         event.preventDefault();
-        if (checkData()){
-        var calculateThis = {
-            'number1': number1,
-            'number2': number2
-        };
-        $.ajax({
+        if (checkData()) {
+            var calculateThis = {
+                'number1': number1,
+                'number2': number2
+            };
+            $.ajax({
                 type: 'POST',
                 url: '/' + operator,
                 data: calculateThis,
                 success: function(data) {
                     var result = data.solution;
                     resetNumbers(result);
+                    currentNumber=2;
 
                 }
             });
         }
-}
-
-function displayCalcString() {
-
-
-}
-
-function resetNumbers(result) {
-    number1 = result;
-    number2 = '';
-    prevOperator = operator;
-    operator = '';
-    currentNumber = 1;
-    $('#display-number1').text(number1);
-    $('#display-operator').text('');
-
-}
-
-function clearNums() {
-
-
-}
-
-function checkData() {
-    if (number1!=''&&number2!=''&&operator!='') {
-        return true;
-    } else {
-        return false;
     }
-}
+
+
+
+    function resetNumbers(num) {
+        number1 = num;
+        number2 = '';
+        operator = '';
+        currentNumber = 1;
+
+        $('#display-number1').text(num);
+        $('#display-operator').text(operator);
+        $('#number').val('0');
+
+    }
+
+    function clearAllNumbers() {
+        resetNumbers('');
+
+    }
+
+    function clearInputOnly() {
+        if (currentNumber==1) {
+            resetNumbers('');
+        } else {
+            resetNumbers(number1);
+            currentNumber=2;
+        }
+    }
+
+    function checkData() {
+        if (number1 != '' && number2 != '' && operator != '') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 
 });
