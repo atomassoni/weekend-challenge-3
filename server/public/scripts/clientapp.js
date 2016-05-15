d1a9c476d255e9a88e3673746efb62d68c8511ee
atomassoni <annetomassoni@gmail.com>
2016-05-15 16:10:52 -0500

    made more like a calculator

--- server/public/scripts/clientapp.js
+++ server/public/scripts/clientapp.js
@@ -1,13 +1,15 @@
 var number1 = '';
 var number2 = '';
 var operator = '';
+var prevOperator = '';
 var operatorDisplay = {
     'add': '+',
     'subtract': '-',
     'multiply': 'x',
     'divide': '/'
 };
-
+var displayCalcString = '';
+var currentNumber = 1;
 
 $(document).ready(function() {
     console.log('ready');
@@ -25,28 +27,30 @@ $(document).ready(function() {
 
         if (buttonPushed.length > 1) {
             operator = buttonPushed;
+            currentNumber++;
+            $('#display-number1').text(number1);
+            $('#display-operator').text(' ' + operatorDisplay[operator] + ' ');
+
         } else {
             if (operator == "") {
                 number1 += buttonPushed;
+                $('#number').val(number1);
+
             } else {
                 number2 += buttonPushed;
+                $('#number').val(number2);
             }
         }
-
-
-        $('#operator').text(operatorDisplay[operator]);
-        $('#number1').val(number1);
-        $('#number2').val(number2);
 
 
     }
 
     function updateNum() {
-        var num = $(this).attr('data-input');
-        if (num == 'number1') {
-            number1 = $('#number1').val();
+
+        if (currentNumber == 1) {
+            number1 = $('#number').val();
         } else {
-            number2 = $('#number2').val();
+            number2 = $('#number').val();
         }
     }
 
@@ -66,6 +70,7 @@ $(document).ready(function() {
 
     function getResult() {
         event.preventDefault();
+        if (checkData()){
         var calculateThis = {
             'number1': number1,
             'number2': number2
@@ -75,11 +80,41 @@ $(document).ready(function() {
                 url: '/' + operator,
                 data: calculateThis,
                 success: function(data) {
+                    var result = data.solution;
+                    resetNumbers(result);
 
-                    $('#result').text(data.solution);
                 }
             });
+        }
+}
 
+function displayCalcString() {
+
+
+}
+
+function resetNumbers(result) {
+    number1 = result;
+    number2 = '';
+    prevOperator = operator;
+    operator = '';
+    currentNumber = 1;
+    $('#display-number1').text(number1);
+    $('#display-operator').text('');
+
+}
+
+function clearNums() {
+
+
+}
+
+function checkData() {
+    if (number1!=''&&number2!=''&&operator!='') {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 });
